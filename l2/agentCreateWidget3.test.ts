7f3c12989c49a42f2041a10a4c04eb1ffea3505f
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2025-12-18 14:17:36 -0300

    update

--- l2/agentCreateWidget3.test.ts
+++
@@ -1,5 +0,0 @@
-/// <mls shortName="agentCreateWidget3" project="102026" enhancement="_blank" folder="" />
-
- import { ICANTest, ICANIntegration, ICANSchema  } from '/_100554_/l2/tsTestAST.js';
- export const integrations: ICANIntegration[] = [];
- export const tests: ICANTest[] = [];
